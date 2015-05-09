5ab4c1763b97a4ae48d7499a5550236ce7da5f8d
barta3 <baertschi.adrian@gmail.com>
2015-05-09 16:14:20 +0200

    cleanup

--- src/main/java/ch/bfh/bti7311/DistributedAlgo/leaderElection/Message.java
+++ src/main/java/ch/bfh/bti7311/DistributedAlgo/leaderElection/Message.java
@@ -1,14 +1,14 @@
 package ch.bfh.bti7311.DistributedAlgo.leaderElection;
 
 public class Message {
-	
-	// TODO: Simplify, remove enum
+
 	private int id;
 	private Type type;
+
 	public enum Type {
-			CANDIDATE_IS, LEADER_IS
+		CANDIDATE_IS, LEADER_IS
 	}
-	
+
 	public Message(Type type, int id) {
 		this.setId(id);
 		this.setType(type);
@@ -29,7 +29,7 @@ public class Message {
 	public void setType(Type type) {
 		this.type = type;
 	}
-	
+
 	@Override
 	public String toString() {
 		return type + " " + id;
--- src/main/java/ch/bfh/bti7311/DistributedAlgo/leaderElection/Node.java
+++ src/main/java/ch/bfh/bti7311/DistributedAlgo/leaderElection/Node.java
@@ -10,26 +10,30 @@ public class Node implements Runnable {
 	private int id;
 	private int currLeader;
 	private Node next;
-		
+
 	private BlockingQueue<Message> queue = new LinkedBlockingQueue<Message>();
-	
+
 	public Node(int value) {
 		this.id = value;
 		this.currLeader = value;
 	}
-	
+
 	public Node getNext() {
 		return next;
 	}
+
 	public void setNext(Node next) {
 		this.next = next;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public void putInQueue(Message m) {
 		try {
 			queue.put(m);
@@ -37,7 +41,7 @@ public class Node implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		return String.format("ID %d next: %d", id, next.getId());
@@ -45,38 +49,37 @@ public class Node implements Runnable {
 
 	public void run() {
 
-		Message m = new Message(Type.CANDIDATE_IS, this.id);
-		this.getNext().putInQueue(m);
+		this.getNext().putInQueue(new Message(Type.CANDIDATE_IS, this.id));
 		boolean done = false;
 
-			Message msgArr;
-			Message msgSend = null;
-			try {
-				while (!done) {
-					msgArr = queue.take();
-					System.out.println(id + " Received " + msgArr);
-					
-					if(msgArr.getType().equals(Type.CANDIDATE_IS)) {
-						if(msgArr.getId() == this.id) {
-							done = true;
-							msgSend = new Message(Type.LEADER_IS, this.currLeader);
-						} else {
-							currLeader = Math.max(id, msgArr.getId());
-							msgSend = new Message(Type.CANDIDATE_IS, currLeader);
-						}
-					} else {
+		Message msgArr;
+		Message msgSend = null;
+		try {
+			while (!done) {
+				msgArr = queue.take();
+				System.out.println(id + " Received " + msgArr);
+
+				if (msgArr.getType().equals(Type.CANDIDATE_IS)) {
+					if (msgArr.getId() == this.id) {
 						done = true;
-						this.currLeader = msgArr.getId();
-						msgSend = new Message(Type.LEADER_IS, msgArr.getId());
+						msgSend = new Message(Type.LEADER_IS, this.currLeader);
+					} else {
+						currLeader = Math.max(id, msgArr.getId());
+						msgSend = new Message(Type.CANDIDATE_IS, currLeader);
 					}
-					System.out.println(id + " Send " + msgSend);
-					this.getNext().putInQueue(msgSend);
-					
+				} else {
+					done = true;
+					this.currLeader = msgArr.getId();
+					msgSend = new Message(Type.LEADER_IS, msgArr.getId());
 				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				System.out.println(id + " Send " + msgSend);
+				this.getNext().putInQueue(msgSend);
+
 			}
-			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		System.out.println(id + " FINISHED - Leader is " + currLeader);
 	}
 
